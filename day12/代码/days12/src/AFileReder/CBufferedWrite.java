1d3d381d9f2f2eb1e31747acc54775f9ffccc6a2
ywz147258 <1094769835@qq.com>
2021-09-14 10:34:47 +0800

    2021年9月14日 10点34分

--- day12/代码/days12/src/AFileReder/CBufferedWrite.java
+++ day12/代码/days12/src/AFileReder/CBufferedWrite.java
@@ -1,19 +1,42 @@
 package AFileReder;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.Writer;
+import java.io.*;
 
 public class CBufferedWrite {
     public static void main(String[] args) throws Exception {
-        Writer writer = new FileWriter("F:\\闽南师范大学\\day12\\练习\\三国演义2.txt");
-        BufferedWriter bw= new BufferedWriter(writer);
+//        Writer writer = new FileWriter("F:\\闽南师范大学\\day12\\练习\\三国演义2.txt");
+//        BufferedWriter bw= new BufferedWriter(writer);
+//        bw.write("ywz");
+//        bw.newLine();
+//        bw.write("hello");
+//        bw.flush();
+//        bw.close();
+//        writer.close();
 
-        bw.write("ywz");
-        bw.newLine();
-        bw.write("hello");
+        copy();
+    }
+
+    /**
+     * 复制
+     * @throws Exception
+     */
+    public static void copy() throws Exception{
+        //读取
+        FileReader fr= new FileReader("F:\\闽南师范大学\\day12\\练习\\三国演义.txt");
+        BufferedReader br= new BufferedReader(fr);
+        //写
+        FileWriter fw = new FileWriter("F:\\闽南师范大学\\day12\\练习\\三国演义1.txt");
+        BufferedWriter bw= new BufferedWriter(fw);
+
+        String result= null;
+        while((result=br.readLine())!=null){
+            bw.write(result);
+            bw.newLine();
+        }
         bw.flush();
-        bw.close();
-        writer.close();
+        bw.close();;
+        fw.close();
+        br.close();
+        fr.close();
     }
 }
