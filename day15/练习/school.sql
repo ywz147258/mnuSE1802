4f3e1753961fbe9d4a8179d7c6ec4d4c75aef674
ywz147258 <1094769835@qq.com>
2021-09-17 10:35:15 +0800

    2021年9月17日 10点35分

---
+++ day15/练习/school.sql
@@ -0,0 +1,107 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 闽师软工18
+ Source Server Type    : MySQL
+ Source Server Version : 50617
+ Source Host           : localhost:3306
+ Source Schema         : school
+
+ Target Server Type    : MySQL
+ Target Server Version : 50617
+ File Encoding         : 65001
+
+ Date: 17/09/2021 10:34:43
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for course
+-- ----------------------------
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE `course`  (
+  `id` int(11) NOT NULL COMMENT '科目表id',
+  `teacher_id` int(11) NULL DEFAULT NULL COMMENT '教师ID  teacher表 id',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '科目名称',
+  `del` tinyint(255) NOT NULL DEFAULT 0 COMMENT '0没删 1删除',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `delete_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of course
+-- ----------------------------
+INSERT INTO `course` VALUES (1, 1, '英语', 0, '2021-09-16 08:44:19', NULL);
+INSERT INTO `course` VALUES (2, 2, '补刀', 0, '2021-09-16 08:44:19', NULL);
+
+-- ----------------------------
+-- Table structure for grade
+-- ----------------------------
+DROP TABLE IF EXISTS `grade`;
+CREATE TABLE `grade`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` int(11) NULL DEFAULT NULL COMMENT '学号 student表id',
+  `course_id` int(11) NULL DEFAULT NULL COMMENT '科目id course 表 id',
+  `score` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '成绩',
+  `del` tinyint(255) NOT NULL DEFAULT 0 COMMENT '0没删 1删除',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `delete_time` datetime(0) NULL DEFAULT NULL COMMENT '删除时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of grade
+-- ----------------------------
+INSERT INTO `grade` VALUES (1, 1, 1, '60', 0, '2021-09-16 08:42:50', NULL);
+INSERT INTO `grade` VALUES (2, 1, 2, '40', 0, '2021-09-16 08:43:24', NULL);
+INSERT INTO `grade` VALUES (3, 2, 1, '50', 0, '2021-09-16 08:43:49', NULL);
+INSERT INTO `grade` VALUES (4, 2, 2, '50', 0, '2021-09-16 10:11:01', NULL);
+INSERT INTO `grade` VALUES (5, 3, 1, '50', 0, '2021-09-16 10:14:29', NULL);
+INSERT INTO `grade` VALUES (6, 1, 1, '80', 0, '2021-09-16 15:41:04', NULL);
+INSERT INTO `grade` VALUES (7, 4, 1, '100', 0, '2021-09-17 09:05:14', NULL);
+
+-- ----------------------------
+-- Table structure for student
+-- ----------------------------
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student`  (
+  `id` int(11) NOT NULL COMMENT '学号',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '学生姓名',
+  `del` tinyint(255) NOT NULL DEFAULT 0 COMMENT '0没删 1删除',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `delete_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of student
+-- ----------------------------
+INSERT INTO `student` VALUES (1, '亚索', 0, '2021-09-16 08:40:55', NULL);
+INSERT INTO `student` VALUES (2, '永恩', 0, '2021-09-16 08:41:10', NULL);
+INSERT INTO `student` VALUES (3, '菲兹', 0, '2021-09-16 08:41:20', NULL);
+INSERT INTO `student` VALUES (4, '安妮', 1, '2021-09-17 09:25:20', '2021-09-17 09:25:35');
+
+-- ----------------------------
+-- Table structure for teacher
+-- ----------------------------
+DROP TABLE IF EXISTS `teacher`;
+CREATE TABLE `teacher`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '教师id',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '教师姓名',
+  `del` tinyint(255) NOT NULL DEFAULT 0 COMMENT '0没删 1删除',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `delete_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of teacher
+-- ----------------------------
+INSERT INTO `teacher` VALUES (1, '游文智', 0, '2021-09-16 08:44:27', NULL);
+INSERT INTO `teacher` VALUES (2, '大司马', 0, '2021-09-16 08:44:27', NULL);
+INSERT INTO `teacher` VALUES (3, '刘德华', 0, '2021-09-16 18:30:57', NULL);
+
+SET FOREIGN_KEY_CHECKS = 1;
