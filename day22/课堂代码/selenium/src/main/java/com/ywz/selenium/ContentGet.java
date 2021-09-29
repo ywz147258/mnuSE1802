302e302f29b598ac50facb2bb3a4c7de8a85bbbc
ywz147258 <1094769835@qq.com>
2021-09-29 10:14:11 +0800

    2021年9月29日 10:14:09

--- day22/课堂代码/selenium/src/main/java/com/ywz/selenium/ContentGet.java
+++ day22/课堂代码/selenium/src/main/java/com/ywz/selenium/ContentGet.java
@@ -7,10 +7,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ContentGet {
     public static void main(String[] args) {
+
+    }
+
+    public void get(){
         File file = new File("C:\\Program Files (x86)\\Google\\Chrome\\Application\\ChromeDriver.exe");
         System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
         ChromeOptions options = new ChromeOptions();
@@ -24,6 +31,22 @@ public class ContentGet {
         }
 
         WebElement content = driver.findElement(By.id("content"));
+
+        String path ="F:\\小说\\封神之我没想和女娲谈恋爱";
+        File file2= new File(path);
+        if(!file2.exists()){
+            file2.mkdirs();
+        }
+        try {
+            FileWriter fw = new FileWriter(new File(path+"\\1.txt"));
+            BufferedWriter bw= new BufferedWriter(fw);
+            bw.write(content.getText());
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         System.out.println(content.getText());
         driver.quit();
     }
