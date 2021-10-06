19e3e33e9addbcad73de838bfae61cc961416e6b
ywz147258 <1094769835@qq.com>
2021-10-06 10:49:23 +0800

    2021年10月4日 10:49:21

--- day23/课堂代码/form/src/main/java/com/ywz/controller/UserController.java
+++ day23/课堂代码/form/src/main/java/com/ywz/controller/UserController.java
@@ -5,6 +5,10 @@ import com.ywz.entity.User;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @RequestMapping("/user")
 @RestController
 public class UserController  {
@@ -14,5 +18,27 @@ public class UserController  {
         UserDao userDao = new UserDao();
         userDao.insert(user);
         return "success";
+    }
+
+    @RequestMapping("/test")
+    public List<User> test(){
+        List<User> list = new ArrayList<>();
+        for(int i =0;i<3;i++){
+            User user = new User();
+            user.setUserName("ywz"+i);
+            user.setPassWord("ps"+i);
+            user.setSex(1);
+            list.add(user);
+        }
+        return list;
+    }
+
+    @RequestMapping("/test2")
+    public User test2(){
+        User user = new User();
+        user.setUserName("ywz");
+        user.setPassWord("ps");
+        user.setSex(1);
+        return user;
     }
 }
