9d3d79aa0d40fb89f7b4226365994d533b67d005
ywz147258 <1094769835@qq.com>
2021-10-12 09:38:49 +0800

    2021年10月12日9:38:47

---
+++ day30/课堂代码/vue002/src/components/AvalueBind.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<div id="">
+			{{value}}
+		</div>
+		<div v-html="name" id="">
+			
+		</div>
+		<div v-bind:class="type" v-bind:id="id">
+			{{type}}
+		</div>
+		<div id="">
+			<input type="text" v-model="name"  />
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+	  data () {
+	    return {
+	      value: 'ywz',
+		  name:"周杰伦",
+		  type:'div',
+		  id:'div'
+	    }
+	  }
+	}
+</script>
+
+<style>
+	.div{
+		border: 1px solid red;
+	}
+	#div{
+		color: red;
+		font-size: 20pt;
+	}
+</style>
