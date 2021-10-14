50c9ecf5675f307cfec83a90fc8bac0ed0c0d08b
ywz147258 <1094769835@qq.com>
2021-10-14 15:16:08 +0800

    2021-10-14 15:16:06

---
+++ day32/课堂代码/demo/src/main/java/com/ywz/controller/FormController.java
@@ -0,0 +1,38 @@
+package com.ywz.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FormController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+        String userName = req.getParameter("userName");
+        String psw =req.getParameter("psw");
+
+        String[] aihao =req.getParameterValues("aihao");
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        out.println("用户名:"+userName+",密码:"+psw);
+        for(String str:aihao){
+            out.println("爱好:"+str);
+        }
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        String userName = req.getParameter("userName");
+        String psw =req.getParameter("psw");
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        out.println("用户名:"+userName+",密码:"+psw);
+    }
+
+
+}
---
+++ day32/课堂代码/demo/src/main/java/com/ywz/controller/InitController.java
@@ -0,0 +1,4 @@
+package com.ywz.controller;
+
+public class InitController {
+}
